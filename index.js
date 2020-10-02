505ad78323864d0be45111d2645095302ca0baeb
yamyam803 <71810067+yamyam803@users.noreply.github.com>
2020-10-02 15:02:33 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// // var Jadwal=Senin,Selasa,Rabu,Kamis,Jumat,Sabtu,Minggu;
+// // while(Jadwal) {
+// //     Jadwal=Senin,Selasa,Rabu,Kamis,Jumat,Sabtu ;
+// //     alert('Dokter yang kamu pilih,Kami cek terlebih dahulu ketersediannya')
+// // }
+
+// var Jadwal = confirm("Dokter yang kamu pilih,Akan kami cek ketersediannya");
+//          if {
+//             document.write("Baiklah,Dokter yang kamu pilih ternyata ada.:)");
+//         }
+
+// onclick {
+//     alert("Dokter yang kamu pilih akan kami cek ketersediannya.")
+// }
